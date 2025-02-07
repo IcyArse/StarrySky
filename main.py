7e1e8ef83230a61b552a2ed77e13590bb92960a1
Icy <yatharthsaroha2003@gmail.com>
2025-02-08 00:54:23 +0530

    load stars and earth data

--- main.py
+++ main.py
@@ -10,3 +10,7 @@ from skyfield.api import Star, load, wgs84
 from skyfield.data import hipparcos
 from skyfield.projections import build_stereographic_projection
 
+ear = load('de421.bsp')
+
+with open(hipparcos.URL) as f:
+        stars = hipparcos.load_dataframe(f)
